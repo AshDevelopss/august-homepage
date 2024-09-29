2f32107fd88447d8dfb90895b4967fca3804cfd5
AshwinManohar <ashwin.manohar@tifin.com>
2024-09-29 16:02:50 +0530

    renamed layout file

--- src/templates/using-dsg.js
+++ src/templates/using-dsg.js
@@ -2,7 +2,7 @@ import * as React from "react"
 import { Link } from "gatsby"
 
 import Seo from "../components/seo"
-import Layout from "../components/Layout"
+import Layout from "../components/layout"
 
 const UsingDSG = () => (
   <Layout>
